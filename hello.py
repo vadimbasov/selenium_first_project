6f475fdee320b0190ca5dbcbf0d042897d3e0247
Vadim Basov <vadim.bassoff@gmail.com>
2026-01-29 21:05:30 +0300

    hello

--- hello.py
+++ hello.py
@@ -5,3 +5,11 @@ for i in range(10):
 
 print(23 + 57)
 
+s = 'My Name is Julia'
+
+if 'Name' in s:
+    print('Substring found')
+
+index = s.find('Name')
+if index != -1:
+    print(f'Substring found at index {index}')
