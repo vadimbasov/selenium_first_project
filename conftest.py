568ac3945fd00465d5d5510327724d9a0f29d7bc
Vadim Basov <vadim.bassoff@gmail.com>
2026-02-04 19:25:38 +0300

    week's work

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+# from selenium.webdriver.common.by import By
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.fixture(autouse=True)
+def prepare_data():
+    print()
+    print("preparing some critical data for every test")
--- test_abs_project.py
+++ test_abs_project.py
@@ -1,10 +1,27 @@
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number"
+import pytest
+from selenium.webdriver.common.by import By
 
-def test_abs2():
-    assert abs(-42) == -42, "Should be absolute value of a number"
 
-if __name__ == "__main__":
-    test_abs1()
-    test_abs2()
-    print("Everything passed")
+class TestMainPage1():
+
+#    @pytest.mark.first
+    @pytest.mark.parametrize('language', ["ru", "en"])
+    def test_guest_should_see_login_link(self, browser, language):
+        print("start test1")
+        link = f"https://{language}.wikipedia.org/wiki/%D0%A1%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0_(%D0%BF%D0%BE%D0%BB%D0%B8%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F)"
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#searchInput")
+        print("finish test1")
+
+#    @pytest.mark.second
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        print("start test2")
+        link = "https://ru.wikipedia.org/wiki/%D0%A1%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0_(%D0%BF%D0%BE%D0%BB%D0%B8%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F)"
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#n-content")
+        print("finish test2")
+
+#    @pytest.mark.xfail(reason="trulala")
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.XPATH, '//*[text()="Пожертвовать"]')
